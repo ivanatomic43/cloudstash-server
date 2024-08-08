33f1b3e1c872f70f42cd1b9ed1fa0a80cc82b18f
ivanatomic43 <ivana.tomic96@gmail.com>
2024-08-08 12:34:29 +0200

    Initialize project

---
+++ src/main/java/com/example/cloudstash/CloudstashApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudstash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudstashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudstashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cloudstash/CloudstashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudstash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudstashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
